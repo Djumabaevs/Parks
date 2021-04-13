9bc67dfc85d2c2656d3cbb5bafb07fcdaa859e25
Djumabaevs <djumabaevb@gmail.com>
2021-04-13 09:25:16 +0600

    util class

--- app/src/main/java/com/bignerdranch/android/parks/data/AsyncResponse.java
+++ app/src/main/java/com/bignerdranch/android/parks/data/AsyncResponse.java
@@ -1,5 +1,10 @@
 package com.bignerdranch.android.parks.data;
 
+import com.bignerdranch.android.parks.model.Park;
+
+import java.util.List;
+
 public interface AsyncResponse {
+    void processPark(List<Park> parks);
 
 }
--- app/src/main/java/com/bignerdranch/android/parks/data/Repository.java
+++ app/src/main/java/com/bignerdranch/android/parks/data/Repository.java
@@ -1,4 +1,30 @@
 package com.bignerdranch.android.parks.data;
 
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.bignerdranch.android.parks.model.Park;
+import com.bignerdranch.android.parks.util.Util;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Repository {
+
+    static List<Park> parkList = new ArrayList<>();
+
+    public static void getParks(final AsyncResponse callback) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, Util.PARKS_URL, null,
+                response -> {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("data");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }, Throwable::printStackTrace);
+    }
 }
