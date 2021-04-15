6529af7b70e11fb30ddc7e7e90b67697331471f5
Djumabaevs <djumabaevb@gmail.com>
2021-04-15 09:30:54 +0600

    api work

--- app/src/main/java/com/bignerdranch/android/parks/DetailsFragment.java
+++ app/src/main/java/com/bignerdranch/android/parks/DetailsFragment.java
@@ -2,15 +2,26 @@ package com.bignerdranch.android.parks;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bignerdranch.android.parks.model.Park;
+import com.bignerdranch.android.parks.model.ParkViewModel;
+
+import java.util.List;
 
 
 public class DetailsFragment extends Fragment {
 
+    private ParkViewModel parkViewModel;
 
     public DetailsFragment() {
 
@@ -25,6 +36,19 @@ public class DetailsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        parkViewModel = new ViewModelProvider(requireActivity()).get(ParkViewModel.class);
+        TextView test = view.getRootView().findViewById(R.id.test);
+        parkViewModel.getSelectedPark().observe(this, new Observer<Park>() {
+            @Override
+            public void onChanged(Park park) {
+                test.setText(park.getName());
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/bignerdranch/android/parks/util/Util.java
+++ app/src/main/java/com/bignerdranch/android/parks/util/Util.java
@@ -1,5 +1,5 @@
 package com.bignerdranch.android.parks.util;
 
 public class Util {
-    public static final String PARKS_URL = "https://developer.nps.gov/api/v1/parks?parkCode=hi&api_key=iMu0z8ZUxhZa5bc5fQw1LNtNbtXZdOwgov01a4Qd";
+    public static final String PARKS_URL = "https://developer.nps.gov/api/v1/parks?parkCode=AZ&api_key=iMu0z8ZUxhZa5bc5fQw1LNtNbtXZdOwgov01a4Qd";
 }
