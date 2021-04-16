ad7cfc442a9a7b909a8a837be34272e42463d903
Djumabaevs <djumabaevb@gmail.com>
2021-04-16 21:36:10 +0600

    fragment layout create

--- app/src/main/java/com/bignerdranch/android/parks/DetailsFragment.java
+++ app/src/main/java/com/bignerdranch/android/parks/DetailsFragment.java
@@ -7,12 +7,15 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bignerdranch.android.parks.adapter.ViewPagerAdapter;
 import com.bignerdranch.android.parks.model.Park;
 import com.bignerdranch.android.parks.model.ParkViewModel;
 
@@ -22,6 +25,8 @@ import java.util.List;
 public class DetailsFragment extends Fragment {
 
     private ParkViewModel parkViewModel;
+    private ViewPagerAdapter viewPagerAdapter;
+    private ViewPager2 viewPager;
 
     public DetailsFragment() {
 
@@ -41,12 +46,27 @@ public class DetailsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        viewPager = view.findViewById(R.id.details_viewpager);
+
+        TextView parkName = view.findViewById(R.id.details_park_name);
+        TextView parkDes = view.findViewById(R.id.details_park_designation);
+
+        TextView description = view.getRootView().findViewById(R.id.details_description);
+        TextView activities = view.getRootView().findViewById(R.id.details_activities);
+        TextView entranceFees = view.getRootView().findViewById(R.id.details_entrancefees);
+        TextView opHours = view.getRootView().findViewById(R.id.details_operatinghours);
+        TextView detailsTopics = view.getRootView().findViewById(R.id.details_topics);
+        TextView directions = view.getRootView().findViewById(R.id.details_directions);
+
         parkViewModel = new ViewModelProvider(requireActivity()).get(ParkViewModel.class);
 
         parkViewModel.getSelectedPark().observe(this, new Observer<Park>() {
             @Override
             public void onChanged(Park park) {
-
+                parkName.setText(park.getName());
+                parkDes.setText(park.getDesignation());
+                viewPagerAdapter = new ViewPagerAdapter(park.getImages());
+                viewPager.setAdapter(viewPagerAdapter);
             }
         });
     }
