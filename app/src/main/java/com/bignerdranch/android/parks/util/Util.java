46486469e8d73acbfb0503b2bda9685f63f63808
Djumabaevs <djumabaevb@gmail.com>
2021-04-18 18:56:08 +0600

    dynamic stateCode

--- app/src/main/java/com/bignerdranch/android/parks/util/Util.java
+++ app/src/main/java/com/bignerdranch/android/parks/util/Util.java
@@ -2,4 +2,8 @@ package com.bignerdranch.android.parks.util;
 
 public class Util {
     public static final String PARKS_URL = "https://developer.nps.gov/api/v1/parks?stateCode=AZ&api_key=iMu0z8ZUxhZa5bc5fQw1LNtNbtXZdOwgov01a4Qd";
+
+    public static String getParksUrl(String stateCode) {
+        return "https://developer.nps.gov/api/v1/parks?stateCode="+stateCode+"&api_key=iMu0z8ZUxhZa5bc5fQw1LNtNbtXZdOwgov01a4Qd";
+    }
 }
